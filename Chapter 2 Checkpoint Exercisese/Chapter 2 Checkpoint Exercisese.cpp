888337a13ef3d4a3da9e5ee09df15e41d32c8f03
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-02-05 17:16:53 -0500

    updated the starting comment

--- Chapter 2 Checkpoint Exercisese/Chapter 2 Checkpoint Exercisese.cpp
+++ Chapter 2 Checkpoint Exercisese/Chapter 2 Checkpoint Exercisese.cpp
@@ -1,4 +1,4 @@
-// Chapter 2 Checkpoint Exercisese.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Chapter 2 Checkpoint Exercisese.cpp : There are three programs in this project, each on a seperate branch. 
 //
 
 #include <iostream>
