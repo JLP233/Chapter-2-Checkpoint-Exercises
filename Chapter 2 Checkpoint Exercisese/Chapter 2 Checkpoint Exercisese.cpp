0b26144f20bd3cd278ad7d237f2a9dc1ba5044d6
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-02-05 19:09:02 -0500

    added code branch for name, address, and phone

--- Chapter 2 Checkpoint Exercisese/Chapter 2 Checkpoint Exercisese.cpp
+++ Chapter 2 Checkpoint Exercisese/Chapter 2 Checkpoint Exercisese.cpp
@@ -1,10 +1,14 @@
-// Chapter 2 Checkpoint Exercisese.cpp : There are three programs in this project, each on a seperate branch. 
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello Class!\n";
+	string name, address, phone;
+	name = "Jeremiah Penney";
+	address = "101 My Street, Newport News, VA";
+	phone = "757-333-4444";
+	cout << name << endl;
+	cout << address << endl;
+	cout << phone << endl;
+	return 0;
 }
 
